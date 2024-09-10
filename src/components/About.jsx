463ee478813f5c5ad8733788db3d41da8a76b622
AfzalAbbasi03 <alltry194@gmail.com>
2024-09-10 16:56:09 +0530

    want to print str

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Lang } from '../utils/langConstants'
 import { LangContext } from '../utils/langContext';
 
 const About = () => {
   const { lang } = useContext(LangContext);
-  const data= Lang[lang]
+  
+  const data= Lang[lang];
+  const str= "ebcaabeeff"
+  const sorted= str.split('').sort();
+  const frequencyMap = {};
+
+  sorted.forEach((char) => {
+    frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+  });
+  
+
   console.log(data)
   console.log(lang)
   return (
     <div className='container mx-auto p-4'>
         <h2 className='text-2xl font-bold'>{data.title}</h2>
-        <p>
+        <p>        
             {data.desc}
         </p>
         
